7329069aa26d16949829ce9494129cb7e0e27ba5
ImashaEdussuriya <135972045+ImashaEdussuriya@users.noreply.github.com>
2023-07-31 02:06:35 +0530

    Practical 05

---
+++ Part A Q1 Do while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 0;
+
+    do{
+        printf("%d\n",1);
+        i++;
+    }while(i<=100);
+}
---
+++ Part A Q1 For.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    for (int x = 0; x <= 100; x++) {
+        printf("%d ", x);
+    }
+
+}
+
+
+
